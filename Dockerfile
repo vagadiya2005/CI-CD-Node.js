f4aa651fa43eaddcb12a182aed58493e4d20ca62
vagadiya2005 <princevagadiya2005@gmail.com>
2024-09-13 21:33:30 +0530

    add prometheous config file

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,13 @@
-# Use an official Node.js runtime as a parent image
 FROM node
 
-# Set the working directory in the container
 WORKDIR /app
 
-# Copy package.json and package-lock.json files to the working directory
 COPY package*.json ./
 
-# Install the app dependencies
 RUN npm install
 
-# Copy the rest of the application code to the working directory
 COPY . .
 
-# Expose the port that your app runs on
-EXPOSE 3000
+EXPOSE 5000
 
-# Define the command to run your app
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,15 +1,64 @@
 const express = require('express');
+const client = require('prom-client');
+
 const port = 5000;
 
 const app = express();
+const register = new client.Registry();
 
-app.get('/',(req,res)=>{
 
-    res.send('Hello World!');
+const httpRequestCounter = new client.Counter({
+    name: 'http_requests_total',
+    help: 'Total number of HTTP requests',
+    labelNames: ['method', 'endpoint', 'status_code'],
+});
+
+register.registerMetric(httpRequestCounter);
+
+client.collectDefaultMetrics({ register });
+
+
+app.use((req, res, next) => {
+    res.on('finish', () => {
+        httpRequestCounter.inc({
+            method: req.method,
+            endpoint: req.path,
+            status_code: res.statusCode,
+        });
+    });
+    next();
 });
 
 
-app.listen(port,()=>{
-console.log(`app is running on port number ${port}`);
+app.get('/metrics', async (req, res) => {
+    res.setHeader('Content-Type', register.contentType);
+    res.end(await register.metrics());
+});
+
+app.get('/', (req, res) => {
+
+    res.send('Welcome to home page');
+});
+
+app.get('/about', (req, res) => {
+
+    res.send('Welcome to about page');
+});
+
+app.get('/other', (req, res) => {
+
+    res.send('Welcome to other page');
+});
+
+app.get('/contact', (req, res) => {
+
+    res.send('Welcome to contact page');
+});
+
+
+
+
+app.listen(port, () => {
+    console.log(`app is running on port number ${port}`);
 
 })
